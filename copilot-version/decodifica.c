ea64dc94b40b4d63c4030d03f7b164e74f34be38
Victor Taveira <rodrigueseti@outlook.com>
2021-11-13 16:37:30 -0300

    Trabalho completo2

---
+++ copilot-version/decodifica.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "src/huffman.c"
+
+
+void decodificar (struct huffman *A, struct tabela_frequencia *tabela, FILE *arq_cod)
+{
+	char c;
+	char frase[512] = "";
+	NoHuff *auxNoHuff = A;
+	Tupla *auxTupla;
+
+	if (A != NULL)
+	{
+		c = fgetc(arq_cod);
+		while (!feof(arq_cod))
+		{
+			auxNoHuff = c == '0' ? auxNoHuff->esq : auxNoHuff->dir;
+
+			if (auxNoHuff->esq == NULL && auxNoHuff->dir == NULL)
+			{
+				auxTupla = getFromIndex(tabela, auxNoHuff->index);
+				strcat(frase, auxTupla->palavra);
+				strcat(frase, " ");
+				auxNoHuff = A;
+			}
+
+			c = fgetc(arq_cod);
+		}
+	}
+	puts(frase);
+}
+
+int main(void)
+{
+	FILE *key = fopen("chave_decodificacao.bin", "rb");
+	if (key != NULL)
+	{
+		FILE *arq_cod = fopen("arquivo_codificado.txt", "r");
+		if (arq_cod != NULL)
+		{
+			Tupla auxRegTupla;
+			Tupla *auxPonteiroTupla;
+			Tupla *ultimo;
+			struct huffman *Arvh;
+			struct tabela_frequencia *tabela = NULL;
+			
+			//////Recriando a tabela de frequencias
+			fread(&auxRegTupla, sizeof(Tupla), 1, key);
+
+			while (!feof(key))
+			{
+				auxPonteiroTupla = malloc(sizeof(Tupla));
+
+				strcpy(auxPonteiroTupla->palavra, auxRegTupla.palavra);
+				auxPonteiroTupla->freq = auxRegTupla.freq;
+				strcpy(auxPonteiroTupla->prefixo, auxRegTupla.prefixo);
+				auxPonteiroTupla->prox = NULL;
+
+				if (tabela == NULL) {
+					tabela = auxPonteiroTupla;
+					ultimo = auxPonteiroTupla;
+				}
+				else {
+					ultimo->prox = auxPonteiroTupla;
+					ultimo = ultimo->prox;
+				}
+				fread(&auxRegTupla, sizeof(Tupla), 1, key);
+			}
+			//////////////////////////////////////////
+
+			Arvh = criaHuffTabela(tabela);
+			decodificar(Arvh, tabela, arq_cod);
+
+			destroiHuffman(&Arvh);
+			destroiTabela(&tabela);
+
+			fclose(arq_cod);
+		}
+		else
+			perror("Erro (ArqTxt):");
+
+		fclose(key);
+	}
+	else
+		perror("Erro (ArqBin):");
+
+	return 0;
+}
---
+++ copilot-version/src/lista_freq.c
@@ -0,0 +1,106 @@
+typedef struct tabela_frequencia
+{
+	char palavra[50];
+	int freq;
+	char prefixo[50]; //Ou codigo
+	struct tabela_frequencia *prox;
+
+} Tupla;
+
+Tupla *criaTuplaFreq(char *p)
+{
+	Tupla *novaTupla = malloc(sizeof(Tupla));
+	strcpy(novaTupla->palavra, p);
+	novaTupla->freq = 1;
+	strcpy(novaTupla->prefixo, "");
+	novaTupla->prox = NULL;
+	return novaTupla;
+}
+
+struct tabela_frequencia *criaTabelaFreq(char frase[])
+{
+	char *p = strtok(frase, " ");
+
+	Tupla *cabeca = criaTuplaFreq(p);
+	Tupla *ultimo = cabeca;
+	Tupla *aux;
+
+	while (p != NULL)
+	{
+		p = strtok(NULL, " ");
+
+		if (p != NULL)
+		{
+			aux = cabeca;
+			while (aux != NULL && strcmp(p, aux->palavra))
+				aux = aux->prox;
+
+			if (aux == NULL)
+			{
+				ultimo->prox = criaTuplaFreq(p);
+				ultimo = ultimo->prox;
+			}
+			else
+				aux->freq++;
+		}
+	}
+	return cabeca;
+}
+
+int getIndex(struct tabela_frequencia *lista, char p[])
+{
+	Tupla *auxTupla = lista;
+
+	int i = 0;
+	while (auxTupla != NULL && strcmp(p, auxTupla->palavra))
+	{
+		auxTupla = auxTupla->prox;
+		i++;
+	}
+
+	if (auxTupla == NULL)
+		return -1;
+	return i;
+}
+
+Tupla *getFromIndex(struct tabela_frequencia *lista, int i)
+{
+	Tupla *auxTupla = lista;
+	while (auxTupla != NULL && i > 0)
+	{
+		auxTupla = auxTupla->prox;
+		i--;
+	}
+
+	if (auxTupla == NULL)
+		return NULL;
+	return auxTupla;
+}
+
+Tupla *getFromPalavra(Tupla *tabela, char p[])
+{
+	Tupla *auxTupla = tabela;
+	while (auxTupla != NULL && strcmp(p, auxTupla->palavra))
+		auxTupla = auxTupla->prox;
+
+	return auxTupla;
+}
+
+void exibeTabela(struct tabela_frequencia *tabela)
+{
+	while (tabela != NULL)
+	{
+		printf("[%s]\t\t[%d]\t\t[%s]\n", tabela->palavra, tabela->freq, tabela->prefixo);
+		tabela = tabela->prox;
+	}
+}
+
+void destroiTabela(struct tabela_frequencia **tabela)
+{
+	if (*tabela != NULL)
+	{
+		destroiTabela(&(*tabela)->prox);
+		free(*tabela);
+		*tabela = NULL;
+	}
+}
